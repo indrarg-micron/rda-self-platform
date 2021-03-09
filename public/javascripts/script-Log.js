ec3d48937b4f756d834ad396421fac52cf7cd264
INDRA RADITYA GUNAWAN <indrarg@micron.com>
2021-03-09 22:19:21 +0800

    check if from date is before or on to date

--- public/javascripts/script-Log.js
+++ public/javascripts/script-Log.js
@@ -2,6 +2,12 @@
 $('#date-range').on('click', function() {
   var from = $('#from').val()
   var to = $('#to').val()
+
+  // check if from date is before or the same as to date
+  if (new Date(from) > new Date(to)) {
+    return alert('End date should be after or on From date')
+  }
+
   var url = `/log?from=${from}&to=${to}`
   if (url) { // require a URL
     window.location = url // redirect
