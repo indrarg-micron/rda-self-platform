ee4f91cdb0aaf4dfbbd73dc7453de11edaec6aa7
INDRA RADITYA GUNAWAN <indrarg@micron.com>
2021-05-04 23:09:26 +0800

    change porting to from lastQ to currQ instead

--- misc.js
+++ misc.js
@@ -7,24 +7,24 @@ const { poolProd535, sqlPath } = require('./db')
 moment.tz.setDefault('Asia/Singapore')
 moment.defaultFormat = 'MMM D, YYYY h:mm a z'
 
-// last 4 Micron Financial Quarters (FQs) and next 1 FQ ~more or less~
+// last 4 Micron Financial Quarters (FQs) ~more or less~
 // accurate up to month (too troublesome to be accurate up to WW)
 let fyq = {
   quarters: [], 
   currQ: '', 
-  nextQ: '', 
+  lastQ: '', 
   quarterList: '', 
   quarterColumn: ''
 }
 
 function getQuarters() {
-  fyq.quarters = [ -7, -4, -1, 2, 5 ].map(i => 
+  fyq.quarters = [ -4, -1, 2, 5 ].map(i => 
     moment().subtract(i, 'M').format('[FY]YY[Q]Q')
   )
-  fyq.currQ = fyq.quarters[1]
-  fyq.nextQ = fyq.quarters[0]
-  fyq.quarterList = `('${fyq.quarters[4]}', '${fyq.quarters[3]}', '${fyq.quarters[2]}', '${fyq.quarters[1]}')`
-  fyq.quarterColumn = `([${fyq.quarters[4]}], [${fyq.quarters[3]}], [${fyq.quarters[2]}], [${fyq.quarters[1]}])`
+  fyq.currQ = fyq.quarters[0]
+  fyq.lastQ = fyq.quarters[1]
+  fyq.quarterList = `('${fyq.quarters[3]}', '${fyq.quarters[2]}', '${fyq.quarters[1]}', '${fyq.quarters[0]}')`
+  fyq.quarterColumn = `([${fyq.quarters[3]}], [${fyq.quarters[2]}], [${fyq.quarters[1]}], [${fyq.quarters[0]}])`
 }
 
 // get quarters when the app start
@@ -41,18 +41,18 @@ cron.schedule('1 0 1 Mar,Jun,Sep,Dec *', () => {
 // port every 1st day of month of FQ, 00:05 AM SGT
 cron.schedule('5 0 1 Mar,Jun,Sep,Dec *', async () => {
   let timestamp = moment().format()
-  console.log(`Porting ${fyq.currQ} score data to ${fyq.nextQ} on ${timestamp}`)
+  console.log(`Porting ${fyq.lastQ} score data to ${fyq.currQ} on ${timestamp}`)
 
   try {
     let query = fs.readFileSync(path.join(sqlPath, 'port-to-next-q.sql')).toString()
-    query = query.replace('###NEXT_QUARTER###', nextQ)
-    query = query.replace('###CURRENT_QUARTER###', currQ)
+    query = query.replace('###NEXT_QUARTER###', currQ)
+    query = query.replace('###CURRENT_QUARTER###', lastQ)
 
     const pool = await poolProd535
     const result = await pool.request()
         .query(query)      
     
-    console.log(result.rowsAffected.pop() + ' rows copied to ' + nextQ)
+    console.log(result.rowsAffected.pop() + ' rows copied to ' + currQ)
   
   } catch (err) {
     console.log(err.message)
